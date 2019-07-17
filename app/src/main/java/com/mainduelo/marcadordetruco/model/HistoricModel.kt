f55be09bb85499f1c05dcbed7ae79ad44537b414
MainDuelo <mainduelo@gmail.com>
2019-07-16 21:51:38 -0300

    Added model

---
+++ app/src/main/java/com/mainduelo/marcadordetruco/model/HistoricModel.kt
@@ -0,0 +1,7 @@
+package com.mainduelo.marcadordetruco.model
+
+class HistoricModel {
+    public var scoreTeam1: String? = null
+    public var scoreTeam2: String? = null
+    public var time: String? = null
+}
